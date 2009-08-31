bb6b8770c863c84715ed840f7fdb7c89dbda80ca
PhiLho <PhiLho@GMX.net>
2009-08-31 14:41:34 +0200

    Creating base Exif Reader, showing information and dumping thumbnails

---
+++ _SmallPrograms/ExifReader/ExifProcessing.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+import java.awt.*;
+import java.awt.image.*;
+
+
+import com.drew.imaging.*;
+import com.drew.imaging.jpeg.*;
+import com.drew.metadata.*;
+import com.drew.metadata.exif.*;
+import com.drew.metadata.iptc.*;
+import com.drew.metadata.jpeg.*;
+
+
+class ExifProcessing
+{
+  static void ShowInformation(Metadata metadata)
+  {
+    Iterator directories = metadata.getDirectoryIterator();
+    while (directories.hasNext())
+    {
+      Directory directory = (Directory) directories.next();
+      // iterate through tags and print to System.out
+      Iterator tags = directory.getTagIterator();
+      while (tags.hasNext())
+      {
+        Tag tag = (Tag) tags.next();
+        // use Tag.toString()
+        System.out.println(tag);
+      }
+    }
+  }
+
+  static void WriteThumbnail(Metadata metadata, File pictureFile)
+  {
+    ExifDirectory dir = (ExifDirectory) metadata.getDirectory(ExifDirectory.class);
+    if (!dir.containsThumbnail())
+    {
+      System.out.println("No thumbnail");
+      return;
+    }
+    String fileName = pictureFile.getName();
+    File file = new File(pictureFile.getParentFile().getPath(),
+        fileName.replaceAll("\\.jpe?g$", ".thumb.jpg"));
+    if (file.getName().equals(fileName))
+    {
+      // Don't overwrite file if something is going wrong!
+      System.out.println("Wrong extension? " + file.getAbsolutePath());
+      return;
+    }
+    try
+    {
+      dir.writeThumbnail(file.getAbsolutePath());
+      System.out.println("Thumbnail written: " + file.getAbsolutePath());
+    }
+    catch (MetadataException me)
+    {
+      System.out.println(me);
+    }
+    catch (IOException ioe)
+    {
+      System.out.println(ioe);
+    }
+  }
+}
---
+++ _SmallPrograms/ExifReader/ExifReader.pde
@@ -0,0 +1,56 @@
+import com.drew.imaging.*;
+import com.drew.imaging.jpeg.*;
+import com.drew.metadata.*;
+import com.drew.metadata.exif.*;
+import com.drew.metadata.iptc.*;
+import com.drew.metadata.jpeg.*;
+
+
+void setup()
+{
+  size(800, 800);
+  background(0);
+
+  String pictureFolder = selectFolder("Image Folder");
+
+  if (pictureFolder == null)
+  {
+    println("Canceled");
+    return;
+  }
+  File pictFolder = new File(pictureFolder);
+  File[] pictureFiles = pictFolder.listFiles(
+      new FilenameFilter()
+      {
+        boolean accept(File dir, String name)
+        {
+          String fn = name.toLowerCase();
+          // Exif is only in these files, no?
+          return fn.endsWith(".jpg") || fn.endsWith(".jpeg");
+        }
+      }
+  );
+  int x = 10, y = 10;
+  int maxHeight = 0;
+  for (int fi = 0; fi < pictureFiles.length; fi++)
+  {
+    File pf = null;
+    Metadata metadata = null;
+    try
+    {
+      pf = pictureFiles[fi];
+      println("\n" + pf);
+      metadata = JpegMetadataReader.readMetadata(pf);
+    }
+    catch (JpegProcessingException ex)
+    {
+      println("Ouch! " + pf.getName());
+    }
+    if (metadata != null)
+    {
+      ExifProcessing.ShowInformation(metadata);
+      ExifProcessing.WriteThumbnail(metadata, pf);
+    }
+  }
+  exit();
+}
