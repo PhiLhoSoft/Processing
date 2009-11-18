7f4959660ee9013bed533429e4bc4741819df5db
PhiLho <PhiLho@GMX.net>
2009-11-18 18:37:48 +0100

    Adding tests: ICS parsing and VFB modification; also a test of proXML

---
+++ _QuickExperiments/Test/EventData.java
@@ -0,0 +1,48 @@
+
+class EventData
+{
+   String dateStart;
+   String timeStart;
+   String dateEnd;
+   String timeEnd;
+   String description;
+   String summary;
+
+   EventData()
+   {
+
+   }
+
+   void ParseLine(String line)
+   {
+      String[] parts = line.split(":");
+      String tag = parts[0];
+      if (tag.equals("DTSTART"))
+      {
+         String[] d = parts[1].split("T");
+         dateStart = d[0];
+         timeStart = d[1];
+      }
+      else if (tag.equals("DTEND"))
+      {
+         String[] d = parts[1].split("T");
+         dateEnd = d[0];
+         timeEnd = d[1];
+      }
+      else if (tag.equals("DESCRIPTION"))
+      {
+         description = parts[1];
+      }
+      else if (tag.equals("SUMMARY"))
+      {
+         summary = parts[1];
+      }
+   }
+
+   @Override
+   public String toString()
+   {
+      return dateStart + " " + timeStart + " to " + dateEnd + " " + timeEnd +
+            " > " + description + " (" + summary + ")";
+   }
+}
---
+++ _QuickExperiments/Test/Test.pde
@@ -0,0 +1,23 @@
+Events events;
+String[] lines;
+int positionCount = 0;
+String file = "events.ics";
+
+//---------------------------------------------------------------------- -------------------
+void setup()
+{
+  size(200, 200);
+  fill(255);
+  noLoop();
+
+  lines = loadStrings(file);
+  events = new Events(lines);
+
+  println("How many events pulled from " + file + "? " + events.GetEventNb());
+
+  for (int i = 0; i < events.GetEventNb(); i++)
+  {
+    EventData e = events.GetEvent(i);
+    println(i + " > " + e);
+  }
+}
